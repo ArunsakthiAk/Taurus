012d5576bb28c9c125706bc96a9053613a0bec2a
ArunsakthiAk <arunsakthi013@gmail.com>
2021-11-21 23:30:31 +0530

    the project is done

---
+++ src/org/com/ComputerModel.java
@@ -0,0 +1,8 @@
+package org.com;
+
+public class ComputerModel {
+public void computerSize() {
+	// TODO Auto-generated method stub
+System.out.println("55inch");
+}
+}
---
+++ src/org/com/Desktop.java
@@ -0,0 +1,14 @@
+package org.com;
+
+public class Desktop extends ComputerModel {
+	private void desktopSize() {
+		// TODO Auto-generated method stub
+System.out.println("32inch");
+	}
+	public static void main(String[] args) {
+		Desktop g = new Desktop();
+		g.desktopSize();
+		g.computerSize();
+	}
+
+}
